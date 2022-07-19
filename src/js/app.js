704216ee34f9d546d22727ce3b66adc20f84d8ac
stevencarle93 <stevencarle@hotmail.com>
2022-07-19 00:17:38 +0000

    Conditional Profile Card Done

--- src/js/app.js
+++ src/js/app.js
@@ -37,13 +37,22 @@ function render(variables = {}) {
           <h1>${variables.name ? variables.name : ""} ${
     variables.lastname ? variables.lastname : ""
   }</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h2>${variables.role ? variables.role : ""}</h2>
+          <h3>${variables.city ? variables.city : ""}</h3>
+          <h3>${variables.country ? variables.country : ""}</h3>
+          <ul class=${
+            variables.socialMediaPosition ? variables.socialMediaPosition : ""
+          }>
+            <li><a href="https://twitter.com/4geeksacademy"><i class="fa fa-twitter"></i></a></li>
+            <li><a href=${
+              variables.github ? variables.github : ""
+            }><i class="fa fa-github"></i></a></li>
+            <li><a href=${
+              variables.linkedin ? variables.linkedin : ""
+            }><i class="fa fa-linkedin"></i></a></li>
+            <li><a href=${
+              variables.instagram ? variables.instagram : ""
+            }><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -65,7 +74,7 @@ window.onload = function() {
     socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: null,
     linkedin: null,
     instagram: null,
     name: null,
